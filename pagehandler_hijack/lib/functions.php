adcb1f8f2cc714864948c504ea2ba404e2d05b0f
Matt Beckett <beck24@gmail.com>
2012-03-18 17:30:18 -0600

    Finishing touches for initial release

--- pagehandler_hijack/lib/functions.php
+++ pagehandler_hijack/lib/functions.php
@@ -34,8 +34,26 @@ function pagehandler_hijack_linkfix($hook, $type, $returnvalue, $params){
 
 // hook called on route, all
 // check if $returnvalue['handler'] to see if we need to replace it
+// if the handler is an original handler, we want to foward it to the new url
 function pagehandler_hijack_route($hook, $type, $returnvalue, $params){
   $handlers = pagehandler_hijack_get_replacements();
+  
+  if(in_array($returnvalue['handler'], array_keys($handlers))){
+    // we have been given an old handler -> we should forward to the replacement
+    // probably from an old link or something
+    $currenturl = current_page_url();
+    
+    //get everything after the pagehandler
+    $afterhandler = str_replace(elgg_get_site_url() . $returnvalue['handler'], "", $currenturl);
+    
+    $newurl = elgg_get_site_url() . $handlers[$returnvalue['handler']] . $afterhandler;
+    
+    // forward to the new url
+    forward($newurl);
+    
+    // prevent code execution after the forward
+    exit;
+  }
   
   if(in_array($returnvalue['handler'], $handlers)){
     // we need to do something about it
